0dfeb365b3ae156d0888a8e6a4eee90c1ba97a45
Kevin <kevinsartiano@hotmail.com>
2019-05-17 23:36:37 +0200

    Add basic Kivy GUI

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import random
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class DeutscheVerben:
+class OpeningWindow(Screen):
+    pass
 
+
+class MainWindow(Screen):
+    pass
+
+
+class ResultWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class DeutscheVerben(App):
     tense_list = ['Präsens (er) ', 'Präteritum (ich) ', 'Perfekt (er) ']
     verb_list = {'Beginnen': ['beginnt', 'begann', 'hat begonnen']}
 
-    def __init__(self):
+    def build(self):
+        return Builder.load_file('deutscheverben.kv')
+
+    def exercise(self):
         random_verb = random.choice(list(self.verb_list.keys()))
         answer_list = []
         mistake_counter = -1
@@ -23,4 +48,4 @@ class DeutscheVerben:
 
 
 if __name__ == '__main__':
-    DeutscheVerben()
+    DeutscheVerben().run()
