ac119a9ba57d6c6822c477df233fdd1d4cb17ca0
Kevin <kevinsartiano@hotmail.com>
2019-05-20 11:00:09 +0200

    Change GUI to BoxLayout

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
